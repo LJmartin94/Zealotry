35ba5dec299dd38da27b8eda6776604cf9920d91
limartin <lindsayjamesmartin94@gmail.com>
2020-08-14 20:03:09 +0200

    14 August 2020, Started a new app to try and learn the basics of Android Studio and building apps with Java. Found an online tutorial, its fairly simple, but watchable at 2x speed and thorough enough. Currently on episode 34 of Caleb Curry's Android App Development Tutorial. Have already briefly looked at: Logging, Layouts, Views (and finding view by ID), casting in Java, View attributes, calling methods on views, including imports, making 'toast' appear, passing intents, making a new activity, and the AndroidManifest.xml. Am intending to finish this tutorial series and then start building something with the basic concepts I've picked up, but for now am just playing along with the examples.

--- app/src/main/java/com/github/LJmartin94/zealotry/MainActivity.java
+++ app/src/main/java/com/github/LJmartin94/zealotry/MainActivity.java
@@ -2,13 +2,34 @@ package com.github.LJmartin94.zealotry;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("lbo_Main", "Main completed successfully");
+    }
+
+    public void launchNewActivity(View v)
+    {
+        //launch new activity
+        Intent i = new Intent(this, NewActivity.class);
+        startActivity(i);
+    }
+
+    public void unusedExample()
+    {
+//        ((Button)findViewById(R.id.someViewName)).setText("This is how to cast a view to a" +
+//        "button and immediately set its text");
+        Toast.makeText(this, "Toast", Toast.LENGTH_LONG).show();
     }
 }
