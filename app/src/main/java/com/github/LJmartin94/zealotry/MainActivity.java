0d905b89834e40982d101f89b777af8b411534f7
limartin <lindsayjamesmartin94@gmail.com>
2020-08-30 17:19:23 +0200

    30 August 2020, Still following along with the Udacity tutorial, now at lesson 2 part 1. Today populated my app with some junk data in a scroll view, will learn how to connect to the internet next lesson.

--- app/src/main/java/com/github/LJmartin94/zealotry/MainActivity.java
+++ app/src/main/java/com/github/LJmartin94/zealotry/MainActivity.java
@@ -24,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
 	 	super.onCreate(savedInstanceState);
 	 	setContentView(R.layout.activity_main);
 	 	setTitle(getString(R.string.act_main));
+	 	fillToyBox();
 	 	day = getDay();
 	 	setDay(day);
 	 	Log.d("lbo_Main", "Main completed successfully");
@@ -88,4 +89,37 @@ public class MainActivity extends AppCompatActivity {
 //        "button and immediately set its text");
         Toast.makeText(this, "Toast", Toast.LENGTH_LONG).show();
     }
+
+    public void fillToyBox()
+	{
+		TextView mToysListTextView;
+
+		/*
+		 * Using findViewById, we get a reference to our TextView from xml. This allows us to
+		 * do things like set the text of the TextView.
+		 */
+		mToysListTextView = (TextView) findViewById(R.id.tv_toy_names);
+
+		/*
+		 * This String array contains names of classic toys. After all, these are toy apps. We
+		 * wanted to create a way to break concepts down into smaller pieces that we thought might
+		 * be a little easier to understand. In each lesson, we'll demonstrate new concepts using a
+		 * toy app (no, sadly every one won't have ACTUAL toys in it) and then we'll guide you
+		 * through adding the functionality that you've just learned by having you use those
+		 * concepts in Sunshine! Let us know what you think! We're really excited to have you
+		 * taking this course.
+		 */
+		String[] toyNames = ToyBox.getToyNames();
+
+		/*
+		 * Iterate through the array and append the Strings to the TextView. The reason why we add
+		 * the "\n\n\n" after the String is to give visual separation between each String in the
+		 * TextView. Later, we'll learn about a better way to display lists of data.
+		 */
+		for (String toyName : toyNames)
+		{
+			mToysListTextView.append(toyName + "\n\n\n");
+		}
+	}
+
 }
