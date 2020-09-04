2542d7d13cd9455a2676699a48d3064e2e351385
limartin <lindsayjamesmartin94@gmail.com>
2020-09-04 12:11:41 +0200

    4 September 2020, Still working through Udacity courses, currently at Lesson 2 part 7. Have learned how to make- and made a 'search' activity button in the menu of the Sunshine activity. Just pushing now as I need to leave for a CAT meeting soon.

--- app/src/main/java/com/github/LJmartin94/zealotry/Sunshine.java
+++ app/src/main/java/com/github/LJmartin94/zealotry/Sunshine.java
@@ -2,9 +2,13 @@ package com.github.LJmartin94.zealotry;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class Sunshine extends AppCompatActivity {
 
@@ -21,5 +25,31 @@ public class Sunshine extends AppCompatActivity {
 		mSearchBoxEditText = (EditText) findViewById(R.id.et_search_box);
 		mUrlDisplayTextView = (TextView) findViewById(R.id.tv_url_display);
 		mSearchResultsTextView = (TextView) findViewById(R.id.tv_github_search_results_json);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.sunshine, menu);
+		return true;
+	}
+
+	// Override onOptionsItemSelected
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item)
+	{
+		// Get the ID of the item that was selected
+		int itemThatWasClickedId = item.getItemId();
+		// If the item's ID is R.id.action_search,
+		if (itemThatWasClickedId == R.id.action_search)
+		{
+			Context context = Sunshine.this;
+			// show a Toast
+			String textToShow = "Search clicked";
+			Toast.makeText(context, textToShow, Toast.LENGTH_SHORT).show();
+			// and return true to tell Android that you've handled this menu click.
+			return true;
+		}
+		// If you do NOT handle the menu click, return super.onOptionsItemSelected to let Android handle the menu click
+		return super.onOptionsItemSelected(item);
 	}
 }
