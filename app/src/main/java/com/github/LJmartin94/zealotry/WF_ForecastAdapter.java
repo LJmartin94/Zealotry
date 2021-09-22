90a720e567694d8968df8d0031d87bc1e5fd0056
limartin <lindsayjamesmartin94@gmail.com>
2021-09-22 14:36:22 +0200

    Changed bracket pairing style because I was getting confused by the scope of classes ad functions. Added private String array to WF_ForecastAdapter class, made it explicitly extend RecyclerView.Adapter class. Started implementing onCreateViewHolder method.

--- app/src/main/java/com/github/LJmartin94/zealotry/WF_ForecastAdapter.java
+++ app/src/main/java/com/github/LJmartin94/zealotry/WF_ForecastAdapter.java
@@ -1,18 +1,31 @@
 package com.github.LJmartin94.zealotry;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.recyclerview.widget.RecyclerView;
 
-public class WF_ForecastAdapter {
+public class WF_ForecastAdapter extends RecyclerView.Adapter<WF_ForecastAdapter.ForecastAdapterViewHolder>
+{
+	private String[] mWeatherData;
 
-	public class ForecastAdapterViewHolder extends RecyclerView.ViewHolder {
+	public class ForecastAdapterViewHolder extends RecyclerView.ViewHolder
+	{
 		public final TextView mWeatherTextView;
 
-		public ForecastAdapterViewHolder(View view) {
+		public ForecastAdapterViewHolder(View view)
+		{
 			super(view);
 			mWeatherTextView = view.findViewById(R.id.tv_weather_data);
 		}
+	}
+
+	@Override
+	public ForecastAdapterViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType)
+	{
+
 	}
 }
