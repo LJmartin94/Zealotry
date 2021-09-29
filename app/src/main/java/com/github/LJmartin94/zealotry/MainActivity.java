774ff2b3b1958c756dcabe98ad7ce443142121c4
limartin <lindsayjamesmartin94@gmail.com>
2021-09-29 11:52:41 +0200

    Deleted a bunch of junk code from previous experimentations

--- app/src/main/java/com/github/LJmartin94/zealotry/MainActivity.java
+++ app/src/main/java/com/github/LJmartin94/zealotry/MainActivity.java
@@ -24,7 +24,6 @@ public class MainActivity extends AppCompatActivity {
 	 	super.onCreate(savedInstanceState);
 	 	setContentView(R.layout.activity_main);
 	 	setTitle(getString(R.string.act_main));
-	 	fillToyBox();
 	 	day = getDay();
 	 	setDay(day);
 	 	Log.d("lbo_Main", "Main completed successfully");
@@ -48,7 +47,8 @@ public class MainActivity extends AppCompatActivity {
 		String today;
 
 		today = getString(R.string.NaN);
-		switch (day) {
+		switch (day)
+		{
 			case Calendar.SUNDAY:
 				today = getString(R.string.Sunday);
 				break;
@@ -85,24 +85,6 @@ public class MainActivity extends AppCompatActivity {
 		startActivity(i);
 	}
 
-	public void launchGitHubSearch(View v)
-	{
-		//launch new activity
-		Intent i = new Intent(this, GitHubSearch.class);
-		String input = ((EditText)findViewById(R.id.inputTextName)).getText().toString();
-		i.putExtra("Extra_info", input);
-		startActivity(i);
-	}
-
-	public void launchNewActivity(View v)
-	{
-		//launch new activity
-		Intent i = new Intent(this, NewActivity.class);
-		String input = ((EditText)findViewById(R.id.inputTextName)).getText().toString();
-		i.putExtra("Extra_info", input);
-		startActivity(i);
-	}
-
 	public void launchMorningMenu(View v)
 	{
 		//launch new activity
@@ -129,46 +111,4 @@ public class MainActivity extends AppCompatActivity {
 		//i.putExtra("Extra_info", input);
 		startActivity(i);
 	}
-
-
-
-	public void unusedExample()
-    {
-//        ((Button)findViewById(R.id.someViewName)).setText("This is how to cast a view to a" +
-//        "button and immediately set its text");
-        Toast.makeText(this, "Toast", Toast.LENGTH_LONG).show();
-    }
-
-    public void fillToyBox()
-	{
-		TextView mToysListTextView;
-
-		/*
-		 * Using findViewById, we get a reference to our TextView from xml. This allows us to
-		 * do things like set the text of the TextView.
-		 */
-		mToysListTextView = (TextView) findViewById(R.id.tv_toy_names);
-
-		/*
-		 * This String array contains names of classic toys. After all, these are toy apps. We
-		 * wanted to create a way to break concepts down into smaller pieces that we thought might
-		 * be a little easier to understand. In each lesson, we'll demonstrate new concepts using a
-		 * toy app (no, sadly every one won't have ACTUAL toys in it) and then we'll guide you
-		 * through adding the functionality that you've just learned by having you use those
-		 * concepts in Sunshine! Let us know what you think! We're really excited to have you
-		 * taking this course.
-		 */
-		String[] toyNames = ToyBox.getToyNames();
-
-		/*
-		 * Iterate through the array and append the Strings to the TextView. The reason why we add
-		 * the "\n\n\n" after the String is to give visual separation between each String in the
-		 * TextView. Later, we'll learn about a better way to display lists of data.
-		 */
-		for (String toyName : toyNames)
-		{
-			mToysListTextView.append(toyName + "\n\n\n");
-		}
-	}
-
 }
