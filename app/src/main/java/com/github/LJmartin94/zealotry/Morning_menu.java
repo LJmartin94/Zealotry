ebcf0df8e5ba69e7b48a68895ae17ad2f66879c7
limartin <lindsayjamesmartin94@gmail.com>
2021-09-30 16:28:48 +0200

    Made the language button launch duolingo and the meditation button launch headspace for now.

--- app/src/main/java/com/github/LJmartin94/zealotry/Morning_menu.java
+++ app/src/main/java/com/github/LJmartin94/zealotry/Morning_menu.java
@@ -2,7 +2,10 @@ package com.github.LJmartin94.zealotry;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class Morning_menu extends AppCompatActivity {
 
@@ -10,5 +13,33 @@ public class Morning_menu extends AppCompatActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_morning_menu);
+	}
+
+	public void launchLanguage(View v)
+	{
+		Intent i = getPackageManager().getLaunchIntentForPackage("com.duolingo");
+		if (i.resolveActivity(getPackageManager()) != null)
+		{
+			startActivity(i);
+		}
+		else
+		{
+			Toast error = Toast.makeText(this, "Please download Duolingo first", Toast.LENGTH_SHORT);
+			error.show();
+		}
+	}
+
+	public void launchMeditation(View v)
+	{
+		Intent i = getPackageManager().getLaunchIntentForPackage("com.getsomeheadspace.android");
+		if (i.resolveActivity(getPackageManager()) != null)
+		{
+			startActivity(i);
+		}
+		else
+		{
+			Toast error = Toast.makeText(this, "Please download Headspace first", Toast.LENGTH_SHORT);
+			error.show();
+		}
 	}
 }
