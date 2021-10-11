4601af8ea9a31840f1485d66704949593b5235c9
limartin <lindsayjamesmartin94@gmail.com>
2021-10-11 22:57:34 +0200

    Making the 'Wake up' button set an alarm to wake up for the next day. Trying to calculate sunrise time based on the users location. Could use an API, but instead trying to implement an algorithm for it locally.

--- app/src/main/java/com/github/LJmartin94/zealotry/Morning_menu.java
+++ app/src/main/java/com/github/LJmartin94/zealotry/Morning_menu.java
@@ -2,6 +2,8 @@ package com.github.LJmartin94.zealotry;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.provider.AlarmClock;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -13,6 +15,42 @@ public class Morning_menu extends AppCompatActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_morning_menu);
+	}
+
+	public void calculateSunrise()
+	{
+		double	hourSunrise;
+		double	minSunrise;
+
+		double	longitude = 1;
+		double	ha = 1;
+		double	eqtime = 1;
+
+		double	utc_sunrise;
+		utc_sunrise = 720 - 4 * (longitude + ha) -eqtime;
+
+
+	}
+
+	public void launchAlarm(View v)
+	{
+		// Alarm should be set for: (time_you_need_to_leave - time_you_need_to_get_ready) at latest, and at (sunrise or 8:00) at the earliest.
+		// Eventually the app should be able to wake the user when they are in their lightesst sleep somewhere between these two extremes.
+		int	hourSunrise;
+		int minSunrise;
+
+		hourSunrise = 8;
+		minSunrise = 0;
+		Intent i = new Intent(AlarmClock.ACTION_SET_ALARM);
+		i.putExtra(AlarmClock.EXTRA_MESSAGE, "Sunrise");
+		i.putExtra(AlarmClock.EXTRA_HOUR, hourSunrise);
+		i.putExtra(AlarmClock.EXTRA_MINUTES, minSunrise);
+		//i.putExtra(AlarmClock.EXTRA_RINGTONE, )
+		//https://developer.android.com/reference/android/provider/AlarmClock#EXTRA_RINGTONE
+		if (i.resolveActivity(getPackageManager()) != null)
+		{
+			startActivity(i);
+		}
 	}
 
 	public void launchLanguage(View v)
