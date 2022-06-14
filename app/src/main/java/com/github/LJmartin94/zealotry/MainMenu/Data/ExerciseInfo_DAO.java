de076f124c733d3d925e3bbbc434bc826b7147b1
limartin <lindsayjamesmartin94@gmail.com>
2022-06-14 16:22:20 +0200

    Added DAO for room's exercise database management.

--- app/src/main/java/com/github/LJmartin94/zealotry/MainMenu/Data/ExerciseInfo_DAO.java
+++ app/src/main/java/com/github/LJmartin94/zealotry/MainMenu/Data/ExerciseInfo_DAO.java
@@ -1,5 +1,32 @@
 package com.github.LJmartin94.zealotry.MainMenu.Data;
 
+//DAO == Data Access Object
+
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+
+import java.util.List;
+
+@Dao
 public interface ExerciseInfo_DAO
 {
+	// allowing the insert of the same word multiple times by passing a conflict
+	// resolution strategy
+
+	//	The @Insert annotation is a special DAO method annotation where you don't
+	//	have to provide any SQL. There are also @Delete and @Update annotations
+	//	for deleting and updating rows.
+
+	@Insert(onConflict = OnConflictStrategy.REPLACE)
+	void insert(ExerciseInfo_entity exercise);
+
+	@Query("DELETE FROM exercise_table")
+	void deleteAll();
+
+	@Query("SELECT * FROM exercise_table ORDER BY ID ASC")
+	LiveData<List<ExerciseInfo_entity>> getAlphabetizedWords();
+
 }
