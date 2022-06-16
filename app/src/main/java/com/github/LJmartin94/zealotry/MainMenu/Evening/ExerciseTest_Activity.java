514dc2c7f00528e1cab010a99cda53b512846d70
limartin <lindsayjamesmartin94@gmail.com>
2022-06-16 13:19:11 +0200

    Made the ExerciseTest activity create the RecyclerView in its onCreate method.

--- app/src/main/java/com/github/LJmartin94/zealotry/MainMenu/Evening/ExerciseTest_Activity.java
+++ app/src/main/java/com/github/LJmartin94/zealotry/MainMenu/Evening/ExerciseTest_Activity.java
@@ -1,6 +1,8 @@
 package com.github.LJmartin94.zealotry.MainMenu.Evening;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
@@ -9,8 +11,13 @@ import com.github.LJmartin94.zealotry.R;
 public class ExerciseTest_Activity extends AppCompatActivity {
 
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	protected void onCreate(Bundle savedInstanceState)
+	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_exercise_test);
+		RecyclerView recyclerView = findViewById(R.id.exerciseRecyclerView);
+		final Exercise_rvAdapter adapter = new Exercise_rvAdapter(new Exercise_rvAdapter.ExerciseDiff());
+		recyclerView.setAdapter(adapter);
+		recyclerView.setLayoutManager(new LinearLayoutManager(this));
 	}
 }
