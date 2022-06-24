cc28022a27cea88a41a2622f10b39d1f43ae9b14
limartin <lindsayjamesmartin94@gmail.com>
2022-06-24 17:12:10 +0200

    Made corner rounding fit the progress bar. Added NAT stat. Tweaked positioning of stat bars and text sizes. Made stat navigation button work.

--- app/src/main/java/com/github/LJmartin94/zealotry/MainMenu/Overview/Overview_menu.java
+++ app/src/main/java/com/github/LJmartin94/zealotry/MainMenu/Overview/Overview_menu.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 
 import com.github.LJmartin94.zealotry.MainMenu.Overview.BackupData.BackupManagement;
+import com.github.LJmartin94.zealotry.MainMenu.Overview.Stats.StatsOverviewPage;
 import com.github.LJmartin94.zealotry.MainMenu.Overview.WeatherForecast.WeatherForecast;
 import com.github.LJmartin94.zealotry.R;
 
@@ -19,6 +20,14 @@ public class Overview_menu extends AppCompatActivity
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_overview_menu);
 	}
+
+	public void launchStatsPage(View v)
+	{
+		//launch new activity
+		Intent i = new Intent(this, StatsOverviewPage.class);
+		startActivity(i);
+	}
+
 
 	public void launchWeatherForecast(View v)
 	{
