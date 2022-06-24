d9f59e48105e3bf5852d414d6ad49a73c3a57bb6
limartin <lindsayjamesmartin94@gmail.com>
2022-06-24 14:11:30 +0200

    Working on displaying user stats in a separate activity.

---
+++ app/src/main/java/com/github/LJmartin94/zealotry/MainMenu/Overview/Stats/StatsOverviewPage.java
@@ -0,0 +1,18 @@
+package com.github.LJmartin94.zealotry.MainMenu.Overview.Stats;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.github.LJmartin94.zealotry.R;
+
+public class StatsOverviewPage extends AppCompatActivity
+{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_stats_overview_page);
+	}
+}
