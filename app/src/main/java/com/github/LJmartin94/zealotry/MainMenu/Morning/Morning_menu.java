d2a8e35a8e799a40c64024e5a17b09409785639d
limartin <lindsayjamesmartin94@gmail.com>
2022-06-28 12:37:25 +0200

    Added shower menu buttons.

--- app/src/main/java/com/github/LJmartin94/zealotry/MainMenu/Morning/Morning_menu.java
+++ app/src/main/java/com/github/LJmartin94/zealotry/MainMenu/Morning/Morning_menu.java
@@ -22,6 +22,8 @@ import android.widget.Toast;
 import com.github.LJmartin94.zealotry.MainMenu.Utils.WebView_Util_Activity;
 import com.github.LJmartin94.zealotry.R;
 
+import java.util.Calendar;
+
 public class Morning_menu extends AppCompatActivity
 {
 	@Override
@@ -101,6 +103,72 @@ public class Morning_menu extends AppCompatActivity
 		ColorFilter filter = new PorterDuffColorFilter(Color.GRAY, PorterDuff.Mode.MULTIPLY);
 		View m = findViewById(R.id.GetUp_Menu);
 		Button b = findViewById(R.id.get_up_button);
+
+		v.setBackgroundTintBlendMode(BlendMode.MULTIPLY);
+		v.setBackgroundColor(Color.GRAY);
+		v.setClickable(false);
+		m.setVisibility(View.GONE);
+		b.getBackground().setColorFilter(filter);
+		b.setClickable(false);
+	}
+
+	public void launchShower(View v)
+	{
+		View getUpMenu = findViewById(R.id.Shower_Menu);
+		if (getUpMenu.getVisibility() == View.GONE)
+			getUpMenu.setVisibility(View.VISIBLE);
+		else
+			getUpMenu.setVisibility(View.GONE);
+
+		int day = Calendar.getInstance().get(Calendar.DAY_OF_WEEK);
+		if (Calendar.getInstance().get(Calendar.HOUR_OF_DAY) < 4)
+			day = (day == 1) ? 7 : day - 1;
+		if (day == Calendar.SATURDAY) //Wash hair on a SATURDAY
+		{
+			findViewById(R.id.WashHair).setVisibility(View.VISIBLE);
+		}
+	}
+
+	public void ShowerBody(View v)
+	{
+		v.setBackgroundTintBlendMode(BlendMode.MULTIPLY);
+		v.setBackgroundColor(Color.GRAY);
+		v.setClickable(false);
+	}
+
+	public void ShowerHair(View v)
+	{
+		v.setBackgroundTintBlendMode(BlendMode.MULTIPLY);
+		v.setBackgroundColor(Color.GRAY);
+		v.setClickable(false);
+	}
+
+	public void ShowerShave(View v)
+	{
+		v.setBackgroundTintBlendMode(BlendMode.MULTIPLY);
+		v.setBackgroundColor(Color.GRAY);
+		v.setClickable(false);
+	}
+
+	public void ShowerDone(View v)
+	{
+		ColorFilter filter = new PorterDuffColorFilter(Color.GRAY, PorterDuff.Mode.MULTIPLY);
+		View m = findViewById(R.id.Shower_Menu);
+		Button b = findViewById(R.id.shower_button);
+
+		v.setBackgroundTintBlendMode(BlendMode.MULTIPLY);
+		v.setBackgroundColor(Color.GRAY);
+		v.setClickable(false);
+		m.setVisibility(View.GONE);
+		b.getBackground().setColorFilter(filter);
+		b.setClickable(false);
+	}
+
+	public void ShowerSkip(View v)
+	{
+		ColorFilter filter = new PorterDuffColorFilter(Color.GRAY, PorterDuff.Mode.MULTIPLY);
+		View m = findViewById(R.id.Shower_Menu);
+		Button b = findViewById(R.id.shower_button);
 
 		v.setBackgroundTintBlendMode(BlendMode.MULTIPLY);
 		v.setBackgroundColor(Color.GRAY);
